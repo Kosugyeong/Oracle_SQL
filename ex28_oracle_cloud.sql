b3cee957336c5d9b328417611cd87d52d5f1e449
Sugyeong Ko <57031319+Kosugyeong@users.noreply.github.com>
2022-05-03 21:47:11 +0900

    Add files via upload

---
+++ ex28_oracle_cloud.sql
@@ -0,0 +1,18 @@
+-- ex28_oracle_cloud.sql
+
+
+/*
+
+    오라클 클라우드 서비스
+    - Oracle Cloud Free Tier
+    - VM(Virtual Machine) >  나중에 웹 수업 > 프로젝트 운영 or 포트폴리오(웹)
+    - Oracle Autonomous Database 2개까지 제공 > 11g > 19c
+    
+    - 무료 서비스 > 1인 사용 적당
+    - 프로젝트 > N인 사용 부족.. > 여러명이서 동시에 붙으면 서버가 죽음 > 다시 시간 나갔다가 들어가면 돌아오긴 함
+    
+    
+    
+    
+
+*/
